11b8e810ca4ff5dfde90ab6e2854239fb9b56e58
Michal Wagrodzki <m.wagrodzki@gmail.com>
2019-05-14 12:48:20 +0200

    missing js

---
+++ static/js/card_table.js
@@ -0,0 +1,46 @@
+var pageNumber = 36;
+var maxPage;
+
+function download(){
+  return fetch('https://api.hearthstonejson.com/v1/30103/enUS/cards.json')
+  .then(function(response) {
+    return response.json();
+  });
+}
+
+function changePage(event){
+    let button = event.target.id;
+    if (button == "previous" && pageNumber>0){
+        pageNumber--;
+    }
+    if (button == "next" && pageNumber < maxPage-1){
+        pageNumber++;
+    }
+    main();
+}
+
+async function main(){
+    let cards = await download();
+
+    cards = cards.filter(function(card) {
+       return ( card.type == "MINION" || card.type == "SPELL" ) && card.cardClass == "MAGE";
+    });
+
+    maxPage = Math.floor(cards.length / 6);
+
+    let display = '';
+
+    let cardList = document.getElementById('cards');
+    let page = document.getElementById('page');
+
+    for(let i=0;i<6;i++){
+        display += cards[i + pageNumber * 6].name;
+        display += `<img src='https://art.hearthstonejson.com/v1/render/latest/enUS/256x/${cards[i + pageNumber * 6].id}.png'>`;
+    }
+
+    cardList.innerHTML = display;
+    page.innerHTML = pageNumber;
+
+}
+
+main();
